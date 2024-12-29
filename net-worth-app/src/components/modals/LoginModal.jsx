dfbf7235fe857826b72245021c01766743adc732
abodell <andybodell9@gmail.com>
2024-12-28 20:52:29 -0500

    Updated dependency array

--- net-worth-app/src/components/modals/LoginModal.jsx
+++ net-worth-app/src/components/modals/LoginModal.jsx
@@ -33,7 +33,7 @@ const LoginModal = () => {
             console.error(err);
             toast.error('Failed to re-authenticate!')
         }
-    })
+    }, [session?.user?.id])
 
     const {open, ready} = usePlaidLink({
         token: linkToken,
@@ -61,13 +61,13 @@ const LoginModal = () => {
         } finally {
             setIsLoading(false);
         }
-    }, [email, password, loginModal, fetchPlaidData])
+    }, [email, password, loginModal])
 
     const onToggle = useCallback( () => {
         if (isLoading) return;
         loginModal.onClose();
         registerModal.onOpen();
-    }, [loginModal, registerModal, isLoading])
+    }, [loginModal, registerModal])
 
     const fetchPlaidData = async (userID) => {
         try {
